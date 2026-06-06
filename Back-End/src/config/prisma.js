d9fc3a2418d6fade59d264f51800ec291b98f3d8
Mate-Mas <equip8.innovalab@gmail.com>
2026-06-06 00:34:56 -0300

    nuevos cambios en vercel.json y prisma.js

--- Back-End/src/config/prisma.js
+++ Back-End/src/config/prisma.js
@@ -1,14 +1,12 @@
 import { PrismaClient } from '@prisma/client';
 
 const prismaClientSingleton = () => {
-    return new PrismaClient({
+    const client = new PrismaClient({
         log: process.env.NODE_ENV === 'development'
             ? ['query', 'error', 'warn']
             : ['error'],
-        datasources: {
-            db: { url: process.env.DATABASE_URL },
-        },
     });
+    return client;
 };
 
 const globalForPrisma = globalThis;
