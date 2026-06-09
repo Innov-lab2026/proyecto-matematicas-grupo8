fbd105edab21228393e7629c7ae13a27be09cc18
MMundanu <mmundanu@yahoo.com>
2026-06-08 22:56:58 -0300

    feat: crearEditarSeccionSchema

---
+++ Back-End/src/validators/seccion.validator.js
@@ -0,0 +1,26 @@
+import {z} from 'zod';
+export const crearEditarSeccionSchema = z.object({
+    nombre: z.string()
+        .min(2, { message: "El nombre es muy corto" })
+        .max(50)
+        .optional(),
+    descripcion: z.string()
+        .min(2, { message: "La descripcion es muy corta" })
+        .max(50)
+        .optional(),
+    grado: z.number()
+        .int("Debe ser un numero entero")
+        .min(1, "El min es 1 grado"),
+    puntosRequeridos: z.number()
+        .int("Debe ser un numero entero")
+        .min(0, "Debe ser mayor o iual a 0")
+        .optional(),
+    puntosRecompensas: z.number()
+        .int("Debe ser un numero entero")
+        .min(0, "Debe ser mayor o iual a 0")
+        .optional(),
+    umbraAprobacion: z.number()
+        .int("Debe ser un numero entero")
+        .min(0, "Debe ser mayor o iual a 0")
+        .optional()
+})
