214760ba8f4ce6727ce2166d0d44f9b75a88cdfd
Romina_Ruiz <romina.ruiz@alumnos.frgp.utn.edu.ar>
2026-06-30 02:13:05 -0300

    Se agrego la tercera seccion de la pagina Nosotros

---
+++ Front-End/src/components/layouts/Nosotros/ThirdSection.jsx
@@ -0,0 +1,22 @@
+import './Nosotros.css';
+
+
+function ThirdSection() {
+
+    return (
+
+        <div className='container-firstSection'> 
+
+            <h1 className="title-thirdSection">
+                Proceso del equipo de desarrollo de MATE+
+            </h1>
+
+        </div>
+
+
+    );
+
+}
+
+
+export default ThirdSection;
--- Front-End/src/pages/Nosotros.jsx
+++ Front-End/src/pages/Nosotros.jsx
@@ -3,6 +3,7 @@ import FirstSection from '../components/layouts/Nosotros/FirstSection';
 import Footer from '../components/layouts/footer/Footer';
 import Header from '../components/layouts/header/Header';
 import SecondSection from '../components/layouts/Nosotros/SecondSection';
+import ThirdSection from '../components/layouts/Nosotros/ThirdSection';
 
 const Nosotros = () =>{
 
@@ -11,6 +12,7 @@ return(
         <Header/>
         <FirstSection/>
         <SecondSection/>
+        <ThirdSection/>
         <Footer/>
     </>
 
